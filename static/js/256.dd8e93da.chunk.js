edf781e98c17d5fa29f95af3ce8e876afc7d998c
IrynaKholod <104202392+IrynaKholod@users.noreply.github.com>
2023-03-18 23:51:49 +0200

    Updates

---
+++ static/js/256.dd8e93da.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwise_growth=self.webpackChunkwise_growth||[]).push([[256],{256:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsxs)("main",{style:{textAlign:"center"},children:[(0,r.jsx)("b",{style:{fontSize:64},children:"404"}),(0,r.jsx)("p",{children:"Sorry, we couldn't find that page :("})]})}}}]);
+//# sourceMappingURL=256.dd8e93da.chunk.js.map
